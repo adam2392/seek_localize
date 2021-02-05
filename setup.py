16a16e6177e94a1dd0073320615cb1d31a659e05
Adam Li <adam2392@gmail.com>
2021-02-05 18:26:24 -0500

    Fix.

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import os
 import sys
 from distutils.core import setup
 
-import numpy
 from setuptools import find_packages
 
 """
@@ -86,7 +85,6 @@ setup(
         "Source": URL,
         "Tracker": URL + "issues",
     },
-    include_dirs=[numpy.get_include()],
     install_requires=REQUIRED_PACKAGES,
     include_package_data=True,
     classifiers=CLASSIFICATION_OF_PACKAGE,
