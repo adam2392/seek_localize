6ad8b6569f168c1c5f48e331c53a9c9f338f0cce
Adam Li <adam2392@gmail.com>
2021-02-21 18:10:17 -0500

    Increment version number.

--- setup.py
+++ setup.py
@@ -53,6 +53,8 @@ CLASSIFICATION_OF_PACKAGE = [
     # that you indicate whether you support Python 2, Python 3 or both.
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: Implementation",
     "Natural Language :: English",
 ]
